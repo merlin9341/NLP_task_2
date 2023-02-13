e1001f6ba54d1ee6319ba99d9cea5e7c8205645c
Merlin9341 <felixmlroughton@gmail.com>
2023-02-13 19:03:54 +0000

    Formatted output

--- watch_next.py
+++ watch_next.py
@@ -18,6 +18,12 @@ class Movie:
 #A list is created to hold the movies
 movies = []
 
+watched_title =  "Planet Hulk"
+
+watched_description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, "\
+    "the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. "\
+    "Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+
 def file_read():
     #the file is read as a list of descritons and titles
     with open("movies.txt", "r") as movies_file:
@@ -40,5 +46,7 @@ def movie_finder(description):
     return list(map(Movie.get_name, top3))
 
 file_read()
+top3 = movie_finder(watched_description)
 
-print(movie_finder("dragon"))
+print(f"The 3 movies that are most similar to {watched_title} are:")
+print("\n".join(top3))
