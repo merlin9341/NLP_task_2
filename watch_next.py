8e8abcda8e1fcdfc633a267aaa0e949f06f43bf5
Merlin9341 <felixmlroughton@gmail.com>
2023-02-13 17:41:32 +0000

    spacy nlp set up

--- watch_next.py
+++ watch_next.py
@@ -1,5 +1,9 @@
 #Task 38 - Semantic Similarity (NLP) - Task 2
 
+#nlp is set up using spacy
+import spacy
+nlp = spacy.load("en_core_web_md")
+
 #A list is created to hold the movies
 movies = []
 
@@ -7,5 +11,3 @@ movies = []
 with open("movies.txt", "r") as movies_file:
     for line in movies_file:
         movies.append(line.strip("\n").split(" :"))
-
-print(movies)
