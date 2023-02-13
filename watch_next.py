881772583d0ec109ed023ab72d5382a9d18e914b
Merlin9341 <felixmlroughton@gmail.com>
2023-02-13 18:13:25 +0000

    movie_finder now creates a list of similarity

--- watch_next.py
+++ watch_next.py
@@ -12,3 +12,11 @@ def file_read():
     with open("movies.txt", "r") as movies_file:
         for line in movies_file:
             movies.append(line.strip("\n").split(" :"))
+
+def movie_finder(description):
+    "returns the movies from the movies list that are most semantically similar to the description provided"
+    #creats a token from the descrition provided
+    description_token = nlp(description)
+
+    #Uses list comprehension to create list of the movies in the movies list and their similarity to the descrition given
+    similarity_list = [[movie[0], nlp(movie[1]).similarity(description_token)] for movie in movies]
